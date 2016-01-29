900afe52e0275257f586a6cc62fcbc9e74d5a223
Jan-Kanty Pawelski <jan.kanty.pawelski@gmail.com>
2016-01-29 23:04:52 +0100

    file and folders clean up

--- gulpfile.js
+++ gulpfile.js
@@ -10,30 +10,32 @@ var babel = require('babelify');
 var sass = require('gulp-sass');
 
 function compile(watch) {
-    var bundler = watchify(browserify('./source/js/app.js', { debug: true }).transform(babel));
+  var bundler = watchify(browserify('./source/scripts/app.js', { debug: true }).transform(babel));
 
-    function rebundle() {
-        bundler.bundle()
-            .on('error', function(err) { console.error(err); this.emit('end'); })
-            .pipe(source('js/app.js'))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({ loadMaps: true }))
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest(outputPath));
-    }
+  function rebundle() {
+    bundler
+      .bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('scripts/app.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest(outputPath));
+  }
 
-    if (watch) {
-        bundler.on('update', function() {
-            console.log('-> bundling...');
-            rebundle();
-        });
-    }
+  if(watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
 
-    rebundle();
+      rebundle();
+    });
+  }
+
+  rebundle();
 }
 
 function watch() {
-    return compile(true);
+  return compile(true);
 };
 
 gulp.task('build', function() { return compile(); });
@@ -42,25 +44,28 @@ gulp.task('watch', function() { return watch(); });
 gulp.task('default', ['watch', 'copy', 'copy:watch', 'sass', 'sass:watch']);
 
 gulp.task('copy', function() {
-    gulp.src('./source/*.html')
-        .pipe(gulp.dest(outputPath));
+  gulp
+    .src('./source/*.html')
+    .pipe(gulp.dest(outputPath));
 
-    gulp.src('./source/extra/**/*.*')
-        .pipe(gulp.dest(outputPath + '/extra'));
+  gulp
+    .src('./source/assets/**/*.*')
+    .pipe(gulp.dest(outputPath + '/assets'));
 });
 
 gulp.task('copy:watch', function() {
-    gulp.watch('./source/*.html', ['copy']);
-    gulp.watch('./source/extra/**/*.*', ['copy']);
+  gulp.watch('./source/*.html', ['copy'])
+  gulp.watch('./source/assets/**/*.*', ['copy']);
 });
 
 
-gulp.task('sass', function () {
-  gulp.src('./source/css/**/*.scss')
+gulp.task('sass', function() {
+  gulp
+    .src('./source/styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(outputPath + '/css'));
+    .pipe(gulp.dest(outputPath + '/styles'));
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./source/css/**/*.scss', ['sass']);
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./source/styles/**/*.scss', ['sass']);
 });
